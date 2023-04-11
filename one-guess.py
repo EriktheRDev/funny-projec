da9b86b524f3596cd5a9ab7feec122f8b4afcc79
Erik P <122167559+EriktheRDev@users.noreply.github.com>
2023-04-11 11:43:52 -0400

    Update and rename mastermind.py to one-guess.py

--- one-guess.py
+++ one-guess.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 sg.theme("DarkGreen7")
 cue_layout = [[sg.Text("MasterMind")], #[0][0]
 [sg.Button("R"), sg.Text("ed   "), sg.Button("O"), sg.Text('range'), sg.Button("Y"), sg.Text('ellow')], [sg.Button("G"), sg.Text('reen'), sg.Button("B"), sg.Text('lue    '), sg.Button("P"), sg.Text('urple')], #[0][1] and [0][2]
-[sg.Text("[_]"),sg.Text("[_]"),sg.Text("[_]"),sg.Text("[_]"), sg.Text("L1")] #[0][3]
+[sg.Text("[_]"),sg.Text("[_]"),sg.Text("[_]"),sg.Text("[_]")] #[0][3]
 ]
 print("MasterMind Log")
 import random
@@ -14,7 +14,6 @@ correct[2] = possibilities[random.randint(0, 5)]
 correct[3] = possibilities[random.randint(0, 5)]
 cue_win = sg.Window(title="MasterMind", layout=cue_layout)
 current_level = 3
-print(correct)
 eventies = ["[_]","[_]","[_]","[_]"]
 spot = 0
 while True:
@@ -25,7 +24,6 @@ while True:
         eventies[spot] = event
         spot += 1
         if spot == 4:
-            print("ding")
             cor_str = ""
             my_str = ""
             my_guess = [eventies[spot - 4], eventies[spot - 3], eventies[spot - 2], eventies[spot - 1]]
@@ -33,9 +31,6 @@ while True:
                 cor_str += str(i)
             for i in my_guess:
                 my_str += str(i)
-            print(cor_str)
-            print(correct)
-            print(my_str)
             if cor_str == my_str:
                 sg.Popup("You Win!!!!!")
                 break
@@ -57,7 +52,7 @@ while True:
                         
                 wrong = len(my_guess)
                 sg.Popup("Sorry! That's Not Right.\nYou Got " + str(num_right_in_str) + " Characters Correct and in Right Place, \n" + str(right_wrong_place) + " Charactersbut in Wrong Place, \n" + str(wrong) + " Characters Wrong.")
-                sg.Popup("GAME OVER\nSOLUTION:" + str(correct)
+                sg.Popup("GAME OVER\nSOLUTION:" + str(correct))
         cue_win.refresh()
         cue_win.close()
         cue_win = sg.Window(title="MasterMind", layout=[[sg.Text("MasterMind")], #[0][0]
